8dc640f3d71be4243df9399731025d37dfa888fb
Usuário Convidado <Guest@l2602micro18.local>
2017-08-17 19:42:40 -0300

    Initial Commit

---
+++ locasfilmes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  locasfilmes
+//
+//  Created by Usuário Convidado on 17/08/17.
+//  Copyright © 2017 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
